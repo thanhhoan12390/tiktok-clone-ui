65f9160145db2bb4ba8b4ed1cbe092a4a58fed95
unknown <thanhhoan12390@gmail.com>
2024-07-28 01:04:47 +0700

    Add PropTypes

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,7 +1,13 @@
+// import React from 'react';
+import PropTypes from 'prop-types';
 import './GlobalStyles.scss';
 
 function GlobalStyles({ children }) {
+    // return React.Children.only(children); // only 1 child
     return children;
 }
 
+GlobalStyles.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 export default GlobalStyles;
