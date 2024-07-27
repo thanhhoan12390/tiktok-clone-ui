188b56a2262e9729f68e862f1d99c89be27bc5a8
unknown <thanhhoan12390@gmail.com>
2024-07-28 01:29:16 +0700

    Rename files

--- src/layouts/HeaderOnlyLayout/index.js
+++ src/layouts/HeaderOnlyLayout/index.js
@@ -1,14 +1,1 @@
-import Header from '~/layouts/components/Header';
-
-function HeaderOnlyLayout({ children }) {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <div className="content">{children}</div>
-            </div>
-        </div>
-    );
-}
-
-export default HeaderOnlyLayout;
+export { default } from './HeaderOnlyLayout';
